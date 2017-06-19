0f90438bde608ecd80bc03457bb62c184ab5cabd
monzalo14 <monzalo14@gmail.com>
2017-06-19 11:53:28 -0500

    amount plots

--- Paper/RScripts/amount_plots.R
+++ Paper/RScripts/amount_plots.R
@@ -18,7 +18,7 @@ vars <- c(vars_truncadas, 'min_ley', 'minley_salcaidos')
 
 df <- readRDS('../DB/observaciones.RDS') %>%
   group_by(modo_termino) %>%
-  mutate_each(funs(trunca99), one_of(vars_truncadas)) %>%
+  mutate_at(vars(one_of(vars_truncadas)), trunca99) %>%
   mutate(minley_salcaidos = min_ley + sueldo*duracion*365)  %>%
   filter(!(modo_termino == 2 & liq_total != 0), 
          !(modo_termino == 4 & liq_total !=0)) # %>%
@@ -26,45 +26,66 @@ df <- readRDS('../DB/observaciones.RDS') %>%
 
 # Preparo los datos para tener la matriz que necesito de modos de termino, nombre de la variable y cantidad promedio.
 df_plot <- df %>%
-  select(one_of(vars), modo_termino) %>%
+  dplyr::select(one_of(vars), modo_termino) %>%
+  mutate(liq_total_pos = ifelse(liq_total>0, liq_total, NA)) %>%
   gather(key = var, value = monto, -modo_termino) %>%
   filter(!is.na(modo_termino)) %>%
   group_by(modo_termino, var) %>%
   summarise(monto = mean(monto, na.rm = T)) %>%
+  mutate(monto = ifelse(is.na(monto), 0, monto/1000)) %>%
   spread(key = modo_termino, value = monto) 
+
+target <- c('c_total', 'min_ley', 'minley_salcaidos', 'liq_total', 'liq_total_pos')
+df_plot <- df_plot[match(target, df_plot$var),]
 
 varnames <- df_plot$var
 
 df_plot <- df_plot %>%
-  select(-var) %>%
+  dplyr::select(-var) %>%
   as.matrix(.)
 
 # Poner nombres bonitos a la matriz que vamos a graficar
-rownames(df_plot) <- c('Amount asked', 'Amount won', 'Min. comp. by law', 'Min. comp. + lost wages')
+rownames(df_plot) <- c('Amount asked', 'Min. comp. by law', 'Min. comp. + lost wages', 'Amount won', 'Amount won (positive)')
 colnames(df_plot) <- c('Conciliation', 'Drop', 'Court ruling', 'Expiry')
 ylim <- range(df_plot)*c(1,1.25)
 
 # Generamos los porcentajes que necesitamos de cada modo de término:
-prop_mt <- df %>% count(modo_termino) %>% 
-  mutate(n = n*100 / nrow(df)) %>%
+prop_mt <- df %>% 
+  ungroup() %>% 
+  count(modo_termino) %>% 
+  mutate(n = n*100 / nrow(df),
+         modo_termino = as.character(modo_termino)) %>%
   filter(!is.na(modo_termino))
 
-prop_mt_leg <- paste0(colnames(df_plot), ' - ',
-                      substr(as.character(prop_mt$n), 1, 5), '%')
+laudos_pos <- df %>% 
+              filter(modo_termino == 3) %>%
+              ungroup() %>%
+              count(liq_total_pos) %>%
+              mutate(n = n*100/nrow(df)) %>%
+              filter(liq_total_pos == '1') %>%
+              rename(modo_termino = liq_total_pos) %>%
+              mutate(modo_termino = '5')
+
+prop_mt <- rbind(prop_mt, laudos_pos)
+
+prop_mt_leg <- paste0(c(colnames(df_plot),
+                        'Winning court ruling'),
+                      ' - ',
+                      substr(as.character(prop_mt$n), 1, 4), '%')
 
 # Defino las gráficas y les pongo legends
 
-par(mar = c(6.1, 5.1, 2.1, 3.1))
+par(mar = c(4.1, 5.1, 2.1, 3.1))
 
 tiff(file = "../Figuras/Amountasked.tiff", width = 3750, height = 2800, units = "px", res = 800) 
 
-barplot(df_plot, 
+barplot(df_plot,
         beside = T, 
         ylim = ylim, 
         col = 1, 
         lwd = 1:2, 
-        angle = c(0, 45, 90, 45), 
-        density = c(0, 20, 20, 30), 
+        angle = c(0, 45, 90, 135, 0), 
+        density = c(0, 20, 20, 30, 35), 
         cex.names = 0.7, 
         cex.axis = 0.7)
 barplot(df_plot, 
@@ -73,8 +94,8 @@ barplot(df_plot,
         ylim = ylim, 
         col = 1, 
         lwd = 1:2, 
-        angle = c(0, 45, 90, 135), 
-        density = c(0, 20, 20, 30), 
+        angle = c(0, 45, 90, 135, 0), 
+        density = c(0, 20, 20, 60, 35), 
         ylab = 'Amounts (in thousands of pesos)',
         cex.lab = 0.8,
         cex.names = 0.7, 
@@ -82,8 +103,8 @@ barplot(df_plot,
 
 legend('top', legend = rownames(df_plot),
        ncol = 2, fill = TRUE, cex = 0.6, col = 1,
-       angle = c(0, 45, 90, 135),
-       density = c(0, 30, 35, 60),
+       angle = c(0, 45, 90, 135, 0),
+       density = c(0, 30, 35, 60, 65),
        inset = c(0.1, -0.3), xpd=TRUE)
 legend('bottom', legend = prop_mt_leg, 
        ncol = 2, cex = 0.55, 
